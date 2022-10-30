703c63d2b16844630a635b52682cebfb27a0c96c
iamrobertsillo <3815924+iamrobertsillo@users.noreply.github.com>
2022-10-30 11:35:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# p5.js-course
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-mrv4m4)
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import stylesheets
+import {setup, draw} from 'p5';
+import './style.css';
+
+// Write Javascript code!
+function setup() {
+  createCanvas(500, 500);
+  background(0)
+}
+setup()
+function draw() {}

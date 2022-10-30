2b0f753a3ffcc64844e23b9ed53f0c306bbf017e
iamrobertsillo <3815924+iamrobertsillo@users.noreply.github.com>
2022-10-30 12:39:26 +0100

    Created the grid function + the p5js setup

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 // Import stylesheets
-import {setup, draw} from 'p5';
+import p5 from 'p5';
 import './style.css';
 
 // Write Javascript code!
-function setup() {
-  createCanvas(500, 500);
-  background(0)
+function createGrid() {
+  const points = [];
+  const count = 5;
+
+  for (let x = 0; x <= count; x++) {
+    for (let y = 0; y <= count; y++) {
+      const u = x / count;
+      const v = y / count;
+      points.push([u, v]);
+    }
+  }
+  return points;
 }
-setup()
-function draw() {}
+
+const sketch = (s) => {
+  s.setup = () => {
+    s.createCanvas(500, 500);
+    s.background(0);
+  };
+  s.draw = () => {};
+};
+const sketchInit = new p5(sketch);
